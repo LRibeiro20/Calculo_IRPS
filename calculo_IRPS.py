8aad4a47b003b58086ed0c358b18326ce4e038f7
Keven Goncalves <keven.jm.goncalves@gmail.com>
2023-10-02 13:14:03 +0200

    refactor(main): add types and main function to verify if is an import

--- calculo_IRPS.py
+++ calculo_IRPS.py
@@ -1,4 +1,7 @@
-def calcular_imposto_renda(salario_bruto, tabela):
+from typing import Any
+
+
+def calcular_imposto_renda(salario_bruto: float, tabela: list[dict[str, Any]], numero_dependentes: int):
     for intervalo in tabela:
         limite_inferior = intervalo['limite_inferior']
         limite_superior = intervalo['limite_superior']
@@ -29,6 +32,7 @@ def calcular_imposto_renda(salario_bruto, tabela):
 
     return 0  # Retorna 0 se o salário não estiver em nenhum dos intervalos especificados
 
+
 # Definir tabela de intervalos e valores correspondentes
 tabela_imposto = [
     {'limite_inferior': 0.0, 'limite_superior': 20249.99, 'coeficiente': 0.0, 'valores_dependentes': [0.00, 0.00, 0.00, 0.00, 0.00]},
@@ -43,16 +47,21 @@ tabela_imposto = [
     {'limite_inferior': 144750.00, 'limite_superior': None, 'coeficiente': 0.32, 'valores_dependentes': [28375.00, 28325.00, 28300.00, 28275.00, 28225.00]},
 ]
 
-# Salário fornecido
-salario_bruto = float(input("Digite o salario bruto mensal: "))
-numero_dependentes = int(input("Digite o numero de dependentes: "))
 
-# Calcular o imposto de renda
-imposto_renda = calcular_imposto_renda(salario_bruto, tabela_imposto)
+def main():
+    # Salário fornecido
+    salario_bruto = float(input("Digite o salario bruto mensal: "))
+    numero_dependentes = int(input("Digite o numero de dependentes: "))
+
+    # Calcular o imposto de renda
+    imposto_renda = calcular_imposto_renda(salario_bruto, tabela_imposto, numero_dependentes)
+
+    # Exibir o resultado
+    if imposto_renda > 0:
+        print(f"O valor do imposto a ser retido é: {imposto_renda:.2f} MT")
+    else:
+        print("O salário não está em nenhum dos intervalos especificados.")
 
 
-# Exibir o resultado
-# if imposto_renda > 0:
-#     print(f"O valor do imposto a ser retido é: {imposto_renda:.2f} MT")
-# else:
-#     print("O salário não está em nenhum dos intervalos especificados.")
+if __name__ == "__main__":
+    main()
